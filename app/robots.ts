295abee909544515b39af739570304d5eb66e529
Sarvarbek Qodirov <ffeodalik@gmail.com>
2025-12-29 19:35:23 +0500

    update:robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://eskiz.uz/sitemap.xml",
+    sitemap: "https://bekzodhoca.uz/sitemap.xml",
   };
 }
